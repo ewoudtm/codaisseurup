8b930322ec1ce181dd31c3bc79df2c1237f999f7
ewoudtm <ewoud@ewoudtm.nl>
2017-03-14 10:41:13 +0100

    added validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,18 @@ class Event < ApplicationRecord
 
   validates :name_type, presence: true
   validates :description_type, presence: true, length: {maximum: 500}
+  validates :starts_at_type, presence: true
+  validates :ends_at_type, presence: true
+  validate :end_after_start
+
+  private
+  def end_after_start
+    return if ends_at.blank? || starts_date.blank?
+
+    if ends_at <= starts_at + 1.day
+      errors.add(:ends_at, "must be after the start date")
+    end
+  end
+
+
 end
