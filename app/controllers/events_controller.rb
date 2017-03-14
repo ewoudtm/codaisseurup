e59c52e9192383bee7b0bc09367f7ac4c0da3138
ewoudtm <ewoud@ewoudtm.nl>
2017-03-14 14:15:45 +0100

    added event views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:show]
 
   def index
-    @events = current_user.event
+    @events = current_user.events
   end
 
   def show
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,15 @@
 class Event < ApplicationRecord
   belongs_to :user
 
-  validates :name_type, presence: true
-  validates :description_type, presence: true, length: {maximum: 500}
-  validates :starts_at_type, presence: true
-  validates :ends_at_type, presence: true
+  validates :name, presence: true
+  validates :description, presence: true, length: {maximum: 500}
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
   validate :end_after_start
 
   private
   def end_after_start
-    return if ends_at.blank? || starts_date.blank?
+    return if ends_at.blank? || starts_at.blank?
 
     if ends_at <= starts_at + 1.day
       errors.add(:ends_at, "must be after the start date")
