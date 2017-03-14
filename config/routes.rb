57c997e1d9901ce54b531b4e63652acaa1072048
ewoudtm <ewoud@ewoudtm.nl>
2017-03-14 11:59:02 +0100

    added routes and users and events controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get 'pages/home'
   root to: 'pages#home'
 
-
   devise_for :users
+
+  resources :users, only: [:show]
+  resources :events
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
